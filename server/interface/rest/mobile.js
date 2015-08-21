ccd0a67b587f747c8f18fe1f732a4a4dffab28a8
root <root@baiyalong.net>
2015-08-20 22:44:18 -0400

    change folders

---
+++ server/interface/rest/mobile.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bai on 2015/8/21.
+ */
+//ÒÆ¶¯ÖÕ¶Ë½Ó¿Ú
+
+//1,login
+
+//2,logout
+
+//3,subscribe
+
+//4,getData
+
+
+//5,pushData
---
+++ server/interface/rest/weixin.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bai on 2015/8/21.
+ */
+//н╒пе╫с©з
+
+//1,login
+
+//2,logout
+
+//3,subscribe
+
+//4,getData
+
+
+//5,pushData
