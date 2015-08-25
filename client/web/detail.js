ac31b63f7ab3d16898300274c4af047d9524fa59
root <root@baiyalong.net>
2015-08-24 22:29:00 -0400

    mapdata

---
+++ client/web/detail.js
@@ -0,0 +1,17 @@
+
+/**
+ * Created by bai on 2015/8/25.
+ */
+
+Template.detail.helpers({});
+
+Template.detail.events({});
+
+Template.detail.onCreated(function () {
+
+});
+
+
+Template.detail.onRendered(function () {
+
+});
