ad087ec27cbfb6eeb5be540bf6f79de96775a3a9
root <root@baiyalong.net>
2015-09-16 06:52:13 -0400

    app

--- client/mobileClient.js
+++ client/mobileClient.js
@@ -5,10 +5,13 @@
 
 Template.mobileClient.helpers({
     appList: function () {
-        return MobileApp.find({}, {sort: {'metadata.timestamp': -1}});
+        return MobileApp.find({}, {sort: {'timestamp': -1}});
     },
-    getPath: function (url) {
-        return url.substring(0, url.indexOf('?'));
+    getPath: function (id) {
+        if (id) {
+            var url = FileFS.findOne({_id: id}).url()
+            return url ? url.substring(0, url.indexOf('?')) : null;
+        }
     },
     getTime: function (timestamp) {
         return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
@@ -17,63 +20,140 @@ Template.mobileClient.helpers({
 
 Template.mobileClient.events({
     'change input:file': function (event, template) {
-        fileS = event.target.files[0];
+        var id = event.target.id;
+        FT[id] = event.target.files[0]
+    },
+    'click .add': function () {
+        $('#deviceType').val('IOS');
+        $('#version').val('');
+        $('#app').val('');
+        $('#logo').val('');
+        $('#conf').val('');
+        FT = {};
+        $('#deviceType').attr('disabled', false)
+        $('#version').attr('disabled', false)
+        $('#app').attr('disabled', false)
+        $('#logo').attr('disabled', false)
+        $('#conf').attr('disabled', false)
+        $('#appModal').modal()
     },
     'click .cancel': function () {
         $('#deviceType').val('IOS');
         $('#version').val('');
-        $('#appFile').val('');
-        fileS = null;
+        $('#app').val('');
+        $('#logo').val('');
+        $('#conf').val('');
+        FT = {};
     },
     'click .save': function () {
         var deviceType = $('#deviceType').val();
         var version = $('#version').val().trim();
-        var file = fileS;
         if (version == '') {
             alert('版本号不能为空！');
             return;
         }
-        if (file == null) {
-            alert('应用文件不能为空！');
-            return;
-        }
-
-        Meteor.call('checkVersion', {deviceType: deviceType, version: version}, function (err, res) {
-            if (err)Util.modal('移动客户端管理', err);
-            else {
-                if (res != null) {
-                    Util.modal('移动客户端管理', '版本号重复！');
-                    return;
-                } else {
-                    var newFile = new FS.File(file);
-                    newFile.metadata = {
-                        deviceType: deviceType,
-                        version: version,
-                        timestamp: new Date()
-                    };
-                    MobileApp.insert(newFile, function (err, fileObj) {
-                        if (err)Util.modal('移动客户端管理', err);
-                        else {
-                            Util.modal('移动客户端管理', '添加成功！');
-                            $('#deviceType').val('IOS');
-                            $('#version').val('');
-                            $('#appFile').val('');
-                            fileS = null;
-                        }
-                        //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-                    });
+        var id = FT.appId;
+        if (!id) {
+            //insert
+            MobileApp.insert({deviceType: deviceType, version: version}, function (err, appId) {
+                if (err)Util.modal('移动客户端管理', err)
+                if (FT.app) {
+                    FileFS.insert(FT.app, function (err, fileObj) {
+                        MobileApp.update({_id: appId}, {$set: {app: fileObj._id}})
+                    })
                 }
+                if (FT.logo) {
+                    FileFS.insert(FT.logo, function (err, fileObj) {
+                        MobileApp.update({_id: appId}, {$set: {logo: fileObj._id}})
+                    })
+                }
+                if (FT.conf) {
+                    FileFS.insert(FT.conf, function (err, fileObj) {
+                        MobileApp.update({_id: appId}, {$set: {conf: fileObj._id}})
+                    })
+                }
+            })
+        }
+        //Meteor.call('appInsert', {deviceType: deviceType, version: version}, function (err, res) {
+        //    if (err)Util.modal('移动客户端管理', err);
+        //    else {
+        //        Util.modal('移动客户端管理', '添加成功！');
+        //        $('#deviceType').val('IOS');
+        //        $('#version').val('');
+        //        $('#app').val('');
+        //        $('#logo').val('');
+        //        $('#conf').val('');
+        //        FT = {};
+        //        $('#deviceType').attr('disabled', false)
+        //        $('#version').attr('disabled', false)
+        //        $('#app').attr('disabled', false)
+        //        $('#logo').attr('disabled', false)
+        //        $('#conf').attr('disabled', false)
+        //
+        //
+        //
+        //    }
+        //});
+        else {
+            //update
+            if (FT.conf) {
+                var app = MobileApp.findOne({_id: id});
+                if (app.conf) {
+                    FileFS.remove({_id: app.conf})
+                }
+                FileFS.insert(FT.conf, function (err, fileObj) {
+                    MobileApp.update({_id: id}, {$set: {conf: fileObj._id}})
+                })
             }
-        });
+
+        }
+        //Meteor.call('appUpdate', {
+        //    id: id, file: FT
+        //}, function (err, res) {
+        //    if (err)Util.modal('移动客户端管理', err);
+        //    else {
+        //        Util.modal('移动客户端管理', '更新成功！');
+        //        $('#deviceType').val('IOS');
+        //        $('#version').val('');
+        //        $('#app').val('');
+        //        $('#logo').val('');
+        //        $('#conf').val('');
+        //        FT = {};
+        //        $('form').attr('id', undefined)
+        //        $('#deviceType').attr('disabled', false)
+        //        $('#version').attr('disabled', false)
+        //        $('#app').attr('disabled', false)
+        //        $('#logo').attr('disabled', false)
+        //        $('#conf').attr('disabled', false)
+        //    }
+        //});
     },
-    'click .remove': function () {
-        MobileApp.remove({_id: this._id}, function (err) {
-            if (err)Util.modal('移动客户端管理', err);
-            else
-                Util.modal('移动客户端管理', '删除成功！');
-        });
+    'click .edit': function () {
+        FT.appId = this._id;
+        $('#deviceType').val(this.deviceType);
+        $('#version').val(this.version);
+        $('#deviceType').attr('disabled', true)
+        $('#version').attr('disabled', true)
+        $('#app').attr('disabled', true)
+        $('#logo').attr('disabled', true)
+        $('#conf').attr('disabled', false)
+        $('#appModal').modal()
     }
-});
+    ,
+    'click .remove': function () {
+        //Meteor.call('appRemove', this._id, function () {
+        //    Util.modal('移动客户端管理', '删除成功！');
+        //})
+        var app = MobileApp.findOne({_id: this._id})
+        if (app) {
+            if (app.app)FileFS.remove({_id: app.app})
+            if (app.logo)FileFS.remove({_id: app.logo})
+            if (app.conf)FileFS.remove({_id: app.conf})
+        }
+        MobileApp.remove({_id: this._id})
+    }
+})
+;
 
 Template.mobileClient.onRendered(function () {
 
@@ -81,7 +161,8 @@ Template.mobileClient.onRendered(function () {
 );
 
 Template.mobileClient.onCreated(function () {
-        fileS = null;
+
+        FT = {}
 
     }
 );
