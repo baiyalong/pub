a6d8d57675c8451f079939e0ecae5c019131bffa
root <root@baiyalong.net>
2015-09-24 23:15:28 -0400

    app

---
+++ client/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bai on 2015/9/25.
+ */
+
+Template.app.helpers({
+
+});
+
+Template.app.events({
+
+});
+
+Template.app.onRendered(function () {
+
+    }
+);
+
+Template.app.onCreated(function () {
+
+    }
+);
