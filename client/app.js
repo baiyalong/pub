8b2974f7637d42cdba06f6b5154c28e3b67f6145
baiyalong <baiyalong@gmail.com>
2015-11-09 00:13:17 -0500

    app

--- client/app.js
+++ client/app.js
@@ -20,7 +20,7 @@ Template.app.onRendered(function () {
                 else {
                     //Session.set(e, res)
                     if (e == 'Android') {
-                        res = window.location.host + res.substring(res.indexOf('/cfs/'))
+                        res = window.location.origin + res.substring(res.indexOf('/cfs/'))
                     }
                     $('a.' + e).attr('href', res)
                 }
