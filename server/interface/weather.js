dbe32f143b434bd54171b655a9bbc88b5d45eef9
baiyalong <baiyalong@gmail.com>
2015-12-04 13:26:26 +0800

    test

--- server/interface/weather.js
+++ server/interface/weather.js
@@ -2,19 +2,6 @@
  * Created by bai on 2015/11/26.
  */
 
-////基础气象数据接口
-//var url = 'http://open.weather.com.cn/data/?areaid=101010100&type=index_f&date=' + moment(new Date()).format('YYYYMMDDHHmm') + '&appid=';
-//var key = keyFn(url + appid)
-//var call = url + appid.substring(0, 6) + '&key=' + key;
-//
-//var test = 'http://open.weather.com.cn/data/?areaid=101010100&type=index_f&date=201512031042&appid=efb1b1&key=0%2B%2BgrhGIFzzgEg49UCgxVdc4K%2BA%3D'
-//HTTP.call('GET', test, function (err, res) {
-//    console.log(err, res)
-//})
-
-
-//常规气象数据接口
-
 weather = {
     appid: 'efb1b150432032da',
     private_key: '5fa7e3_SmartWeatherAPI_96bd783',
@@ -40,6 +27,6 @@ weather = {
 
 //
 //weather.getData([101010100], weather.type[0], new Date());
-//weather.getData([101010100], weather.type[1], new Date());
+weather.getData([101010100|101020100|101030100], weather.type[1], new Date());
 //weather.getData([101010100], weather.type[2], new Date());
 //weather.getData([101010100], weather.type[3], new Date());
