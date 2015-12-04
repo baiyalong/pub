540364fed7d149152a607a5ff512095d44439997
root <root@localhost.localdomain>
2015-12-04 12:58:19 +0800

    mysql

--- server/lib/mysql.js
+++ server/lib/mysql.js
@@ -1,21 +1,28 @@
-////mysql connect
-//
-//mysql = Mysql.connect({
-//    host: 'localhost',
-//    user: 'root',
-//    password: '',
-//    database: 'NMHBSource'
-//});
-//
-//
-////mysql tables
-//
-//PollutantInfo = mysql.meteorCollection("POLLUTANT", "PollutantInfo");
-//MonitorStationInfo = mysql.meteorCollection("T_ENV_AUTOMONI_AIRSTATIONINFO", "MonitorStationInfo");
-//
-//CityDailyRaw = mysql.meteorCollection("AIR_CITYDAY_AQI_SRC", "CityDailyRaw");
-//CityDailyAudit = mysql.meteorCollection("AIR_CITYDAY_AQI_APP", "CityDailyAudit");
-//
-//StationHourlyRaw = mysql.meteorCollection("T_ENV_AUTOMONI_AIRDATA_HOUR_S", "StationHourlyRaw");
-//StationHourlyAudit = mysql.meteorCollection("T_ENV_AUTOMONI_AIRDATA_HOUR", "StationHourlyAudit");
-//
+//mysql connect
+
+mysql = Mysql.connect({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'NMHBSource'
+});
+
+
+//mysql tables
+
+PollutantInfo = mysql.meteorCollection("POLLUTANT", "PollutantInfo");
+MonitorStationInfo = mysql.meteorCollection("T_ENV_AUTOMONI_AIRSTATIONINFO", "MonitorStationInfo");
+
+CityDailyRaw = mysql.meteorCollection("AIR_CITYDAY_AQI_SRC", "CityDailyRaw");
+CityDailyAudit = mysql.meteorCollection("AIR_CITYDAY_AQI_APP", "CityDailyAudit");
+
+CityHourlyRaw = mysql.meteorCollection("AIR_CITYHOUR_AQI_DATA","CityHourlyRaw");
+
+StationDailyRaw = mysql.meteorCollection("AIR_STATIONDAY_AQI_SRC","StationDailyRaw")
+StationDailyAudit = mysql.meteorCollection("AIR_STATIONDAY_AQI_APP","StationDailyAudit")
+
+StationHourlyRaw = mysql.meteorCollection("T_ENV_AUTOMONI_AIRDATA_HOUR_S", "StationHourlyRaw");
+StationHourlyAudit = mysql.meteorCollection("T_ENV_AUTOMONI_AIRDATA_HOUR", "StationHourlyAudit");
+
+
+
